e9adc7d17e508d68ba96250c88cbf7820d38618e
Xavier Lozano Carreras <xavier.lozano.carreras@a8c.com>
2022-05-06 21:10:22 +0200

    Example working on react-router v5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { useLocation } from 'react-router-dom';
+import {
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { matchRoutes } from 'react-router-dom-v5-compat';
 
-function App() {
+export default function App() {
+  const location = useLocation();
+  const showingModal = matchRoutes([{path: '/modal'}], location.pathname)?.length > 0;
+  const [baseLocation, setBaseLocation] = useState(
+    showingModal ? {
+      pathname: '/',
+      search: '',
+      state: {},
+      hash: '',
+    } : location,
+  );
+
+  useEffect(() => {
+    if (!showingModal) {
+      setBaseLocation(location);
+    }
+  }, [location, location.pathname]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/modal">Modal</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <main>
+          <Switch location={baseLocation}>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
+
+          {showingModal && <Switch location={location}>
+            <Route path="/modal">
+              <Modal />
+            </Route>
+          </Switch>}
+        </main>
+      </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
     </div>
   );
 }
 
-export default App;
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className='modal'>
+      <h2>Modal</h2>
+    </div>
+  );
+}
